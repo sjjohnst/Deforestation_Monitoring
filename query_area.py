a92120a35e49fddaf21753edb353ccafb6e2c6df
Sam <sjjohnst@uwaterloo.ca>
2022-10-06 16:45:17 -0400

    Area selection in matplotlib

--- query_area.py
+++ query_area.py
@@ -6,17 +6,31 @@ Created on Thu Oct  6 15:53:12 2022
 Get the user's area of interest
 """
 
-import folium
-from flask import Flask
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    start_coords = (-14.235004, -51.92528)
-    folium_map = folium.Map(location=start_coords, zoom_start=5)
-    return folium_map._repr_html_()
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.widgets  import RectangleSelector
 
 
-if __name__ == '__main__':
-    app.run(debug=False)
+xdata = np.linspace(0,9*np.pi, num=301)
+ydata = np.sin(xdata)
+
+fig, ax = plt.subplots()
+line, = ax.plot(xdata, ydata)
+
+
+def line_select_callback(eclick, erelease):
+    x1, y1 = eclick.xdata, eclick.ydata
+    x2, y2 = erelease.xdata, erelease.ydata
+
+    rect = plt.Rectangle( (min(x1,x2),min(y1,y2)), np.abs(x1-x2), np.abs(y1-y2) )
+    ax.add_patch(rect)
+
+
+rs = RectangleSelector(ax, line_select_callback,
+                       useblit=False, button=[1], 
+                       minspanx=5, minspany=5, spancoords='pixels', 
+                       interactive=True)
+
+
+
+plt.show()
