aef268ae3d8f2b774aa44d37ec7ba9dad88177a7
Sam <sjjohnst@uwaterloo.ca>
2022-10-13 11:08:53 -0400

    Added Feature: draw query rectangles onto GPD map

--- query_area.py
+++ query_area.py
@@ -6,17 +6,22 @@ Created on Thu Oct  6 15:53:12 2022
 Get the user's area of interest
 """
 
+import geopandas as gpd
 import matplotlib.pyplot as plt
 import numpy as np
 from matplotlib.widgets  import RectangleSelector
 
-
-xdata = np.linspace(0,9*np.pi, num=301)
-ydata = np.sin(xdata)
+world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+brazil = world[world.name == "Brazil"]
+# manaus = gpd.read_file('manaus.geojson')
 
 fig, ax = plt.subplots()
-line, = ax.plot(xdata, ydata)
+world.plot(ax=ax)
+brazil.plot(ax=ax, color='green')
+# manaus.plot(ax=ax, color='yellow', alpha=0.5)
 
+ax.set_xlim((-80, -30))
+ax.set_ylim((-40, 10))
 
 def line_select_callback(eclick, erelease):
     x1, y1 = eclick.xdata, eclick.ydata
@@ -24,13 +29,31 @@ def line_select_callback(eclick, erelease):
 
     rect = plt.Rectangle( (min(x1,x2),min(y1,y2)), np.abs(x1-x2), np.abs(y1-y2) )
     ax.add_patch(rect)
+    
+# Function to be executed after selection
+def onselect_function(eclick, erelease):
+   
+    # Obtain (xmin, xmax, ymin, ymax) values
+    # for rectangle selector box using extent attribute.
+    extent = rect_selector.extents
+    print("Extents: ", extent)
+ 
+    # Zoom the selected part
+    # Set xlim range for plot as xmin to xmax
+    # of rectangle selector box.
+    # plt.xlim(extent[0], extent[1])
+     
+    # Set ylim range for plot as ymin to ymax
+    # of rectangle selector box.
+    # plt.ylim(extent[2], extent[3])
 
 
-rs = RectangleSelector(ax, line_select_callback,
-                       useblit=False, button=[1], 
-                       minspanx=5, minspany=5, spancoords='pixels', 
-                       interactive=True)
+# rs = RectangleSelector(ax, line_select_callback,
+#                        useblit=False, button=[1], 
+#                        minspanx=5, minspany=5, spancoords='pixels', 
+#                        interactive=True)
 
-
+rect_selector = RectangleSelector(
+    ax, onselect_function, drawtype='box', button=[1])
 
 plt.show()
